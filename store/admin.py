6865f83a3539db607c39a4992e358dbf2f425c48
VsesLom <vyacheslavlomov@gmail.com>
2023-07-07 01:04:53 +0300

    Add test task.

--- store/admin.py
+++ store/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Category, Product, Discount, Cart
+from .models import Category, Product, Discount, Cart, WishList
 
 admin.site.register(Category)
 admin.site.register(Product)
 admin.site.register(Discount)
 admin.site.register(Cart)
+admin.site.register(WishList)
 
--- store/urls.py
+++ store/urls.py
@@ -1,15 +1,24 @@
 from django.urls import path
-from .views import ShopView, CartView, ProductSingleView, CartViewSet
+from .views import ShopView, CartView, WishListView, ProductSingleView, CartViewSet, WishListViewSet, add_product, delete_product
 from rest_framework import routers
 
 
-router = routers.DefaultRouter()
-router.register(r'cart', CartViewSet)
+cart_router = routers.DefaultRouter()
+cart_router.register(r'cart', CartViewSet)
+
+wish_router = routers.DefaultRouter()
+wish_router.register(r'wishlist', WishListViewSet)
 
 app_name = 'store'
 
 urlpatterns = [
     path('', ShopView.as_view(), name='shop'),
-    path('cart/', CartView.as_view(), name='cart'),
+    path('category/<int:id>/', ShopView.as_view(), name='category'),
     path('product/<int:id>/', ProductSingleView.as_view(), name='product'),
+    path('cart/', CartView.as_view(), name='cart'),
+    path('<str:section>/add/<int:id>/', add_product, name='add_cart'),
+    path('<str:section>/delete/<int:id>/', delete_product, name='delete_cart'),
+    path('wishlist/', WishListView.as_view(), name='wishlist'),
+    path('<str:section>/add/<int:id>/', add_product, name='add_wishlist'),
+    path('<str:section>/delete/<int:id>/', delete_product, name='delete_wishlist'),
 ]
